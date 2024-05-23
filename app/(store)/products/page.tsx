9708b9a584d5e2f1f9c8881104fb3525096896f8
Franco Feuilles <francofeuilles@gmail.com>
2024-05-23 16:03:57 -0300

    Carga dinámica de productos haciendo uso de los datos de un arreglo de productos

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -2,6 +2,81 @@
 import { useState } from 'react';
 import Pagination from '../../ui/pagination';
 
+const products = [
+    {
+        id: 1,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 2,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 3,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 4,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 5,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 6,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 7,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 8,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 9,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 10,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 11,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+        id: 12,
+        name: "Shoes",
+        description: "If a dog chews shoes whose shoes does he choose?",
+        image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+];
+
 export default function Products() {
     const [selectedPage, setSelectedPage] = useState(1);
 
@@ -14,47 +89,21 @@ export default function Products() {
     return (
         <div className="min-h-screen flex flex-col">
             <h1>Products</h1>
-            <div className="grid m-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 flex-grow">
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+            <div className="grid m-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 flex-grow">
+                {products.map((product) => (
+                    <div key={product.id} className="card w-full bg-base-100 shadow-xl">
+                        <figure>
+                            <img src={product.image} alt={product.name} />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{product.name}</h2>
+                            <p>{product.description}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
             <Pagination totalPages={4} currentPage={selectedPage} onPageChange={handlePageChange} />
         </div>
