23fadcaa4a2396aa46c4f5f1310e4ef9e830837b
JuanOcaranza <68519407+JuanOcaranza@users.noreply.github.com>
2024-05-24 08:02:32 -0300

    Search

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,42 +1,35 @@
 import NavLinks from "@/components/navbar/nav-links"
 import HomeLink from "@/components/navbar/home-link"
+import Search from "@/components/search"
 
 export default function NavBar() {
     return (
-        <div className="navbar bg-orange-600">
-            <div className="navbar-start">
-                <HomeLink />
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
-                        <NavLinks />
-                    </ul>
+        <div className="navbar bg-orange-600 gap-x-4">
+            <HomeLink />
+            <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
                     <NavLinks />
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Categorías</summary>
-                            <ul className="p-2">
-                                <li><a>Caja Navideña</a></li>
-                                <li><a>Caja &quot;Arma Tu Casa&quot;</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
                 </ul>
             </div>
-            <div className="navbar-end">
-                <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered w-56 md:w-auto" />
-                </div>
-                <a className="btn">Button</a>
-            </div>
+            <ul className="menu menu-horizontal px-1 hidden lg:flex">
+                <NavLinks />
+                <li><a>Item 1</a></li>
+                <li>
+                    <details>
+                        <summary>Categorías</summary>
+                        <ul className="p-2">
+                            <li><a>Caja Navideña</a></li>
+                            <li><a>Caja &quot;Arma Tu Casa&quot;</a></li>
+                        </ul>
+                    </details>
+                </li>
+                <li><a>Item 3</a></li>
+            </ul>
+            <Search placeholder="Search..."/>
+            <a className="btn">Button</a>
         </div>
     )
 }
--- lib/data.ts
+++ lib/data.ts
@@ -11,13 +11,13 @@ type apiBox = {
 export async function getBoxes() : Promise<Array<Box>> {
     return await fetch('https://6633d985f7d50bbd9b4ae178.mockapi.io/api/boxes')
     .then(res => res.json())
-    .then(data => data.map((item : apiBox) => ({...item, products: generateRandomProducts()})));
+    .then(data => Array.isArray(data) ? data.map((item : apiBox) => ({...item, products: generateRandomProducts()})) : []);
 }
 
 export async function getFilteredBoxes(query: string, currentPage: number) : Promise<Array<Box>> {
     return await fetch(`https://6633d985f7d50bbd9b4ae178.mockapi.io/api/boxes?search=${query}&page=${currentPage}&limit=12`)
     .then(res => res.json())
-    .then(data => data.map((item : apiBox) => ({...item, products: generateRandomProducts()})));
+    .then(data => Array.isArray(data) ? data.map((item : apiBox) => ({...item, products: generateRandomProducts()})) : []);
 }
     
 
