d7bf029d783a98dcf72b31c75db41907b9d828d1
Franco Feuilles <francofeuilles@gmail.com>
2024-06-26 11:32:28 -0300

    Add max length in Search Component

--- components/navbar/link-search.tsx
+++ components/navbar/link-search.tsx
@@ -33,6 +33,7 @@ export default function LinkSearch({ placeholder }: { placeholder: string }) {
                         setValue(e.target.value);
                     }}
                     onKeyDown={handleKeyDown}
+                    maxLength={100}
                 />
                 <MagnifyingGlassIcon
                     className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
