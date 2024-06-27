0f4cfd549d4ef932a611eeb18f90a0c1c28fe453
JuanOcaranza <68519407+JuanOcaranza@users.noreply.github.com>
2024-06-27 00:45:02 -0300

    Offers carosels

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,33 +1,29 @@
-import HomeCarousel from "@/components/home/home-carousel";
+import HomeCarousels from "@/components/home/home-carousels";
 import CldImage from "@/components/ui/CldImage";
 import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Home',
+    title: 'Home',
 };
 
 export default function Home() {
-  return (
-    <div>
-      <div className="relative w-full h-screen">
-        <CldImage src="slsx5xvcbib7rhouofab" alt="Main Banner" className="w-full h-full object-cover" height={500} width={400} sizes="100vw" />
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-          <div className="bg-black bg-opacity-75 p-4 mb-4 rounded">
-            <h1 className="text-4xl lg:text-6xl font-bold text-white text-center">
-              Welcome to Surprise Boxes!
-            </h1>
-          </div>
-          <Link href="/products" className="bg-white text-black font-semibold hover:bg-black hover:text-white py-2 px-4 text-lg rounded-lg border-2 border-black mt-4">
-            SHOP NOW
-          </Link>
+    return (
+        <div>
+            <div className="relative w-full h-screen">
+                <CldImage src="slsx5xvcbib7rhouofab" alt="Main Banner" className="w-full h-full object-cover" height={500} width={400} sizes="100vw" />
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+                    <div className="bg-black bg-opacity-75 p-4 mb-4 rounded">
+                        <h1 className="text-4xl lg:text-6xl font-bold text-white text-center">
+                            Welcome to Surprise Boxes!
+                        </h1>
+                    </div>
+                    <Link href="/products" className="bg-white text-black font-semibold hover:bg-black hover:text-white py-2 px-4 text-lg rounded-lg border-2 border-black mt-4">
+                        SHOP NOW
+                    </Link>
+                </div>
+            </div>
+            <HomeCarousels />
         </div>
-      </div>
-      <div className="py-10">
-        <div className="container mx-auto">
-          <HomeCarousel />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- components/home/boxes-carousel.tsx
+++ components/home/boxes-carousel.tsx
@@ -1,17 +1,19 @@
 import { Carousel, CarouselContent, CarouselPrevious, CarouselNext, CarouselItem } from "@/components/ui/carousel";
 import BoxCard from "@/components/products/box-card";
-import { getFeaturedBoxes } from '@/lib/data/boxes';
+import { Box } from "@/lib/definitions";
+import { formatDateToLocal } from "@/lib/utils";
 
-export default async function HomeCarousel() {
-    const featuredBoxes = await getFeaturedBoxes();
-
-    return (
-        <div className="w-full my-6 relative flex flex-col items-center justify-center">
-            <h1 className="text-3xl mb-4 hidden md:block w-full text-left">Featured</h1>
+export default async function BoxesCarousel({ name, boxes, endDate }: { name: string, boxes: Box[], endDate?: Date }) {
+return (
+        <div className="w-full my-6 relative flex flex-col items-center justify-center container">
+            <div className="flex flex-col md:flex-row w-full justify-between items-center mb-4">
+                <h1 className="text-3xl">{name}</h1>
+                {endDate && <h2 className="text-lg text-orange-500">Until {formatDateToLocal(endDate.toString())}</h2>}
+            </div>
             <div className="relative w-[98%]">
                 <Carousel className="w-full items-center">
                     <CarouselContent className="-ml-1 gap-4">
-                            {featuredBoxes.map(box => (
+                            {boxes.map(box => (
                                 <CarouselItem key={box.id} className="pl-1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
                                     <BoxCard box={box} />
                                 </CarouselItem>
